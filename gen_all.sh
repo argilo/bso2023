5f3797bd71d02bab962bfaa369d6bb04fc15184b
Clayton Smith <argilo@gmail.com>
2023-10-22 20:02:03 -0400

    Add paint challenge

--- gen_all.sh
+++ gen_all.sh
@@ -17,6 +17,20 @@ grcc hop.grc
 ./hop.py
 cd ..
 
-cat fm/dtmf.sigmf-data hop/hop.sigmf-data > out.sc16
+cd paint
+grcc paint_wide.grc
+convert \
+  -background Black \
+  -gravity Center \
+  -pointsize 600 \
+  -fill White \
+  label:'flag\{this_is_a_very_wide_flag_indeed_c928359da12a4a00e32fa43063e5037256c571a0e75ff2019a6741215a166ff9\}' \
+  -extent 40960x \
+  -resize 4096x\! \
+  flag.png
+./paint_wide.py
+cd ..
+
+cat fm/dtmf.sigmf-data hop/hop.sigmf-data paint/paint.sigmf-data > out.sc16
 
 grcc transmit.grc
